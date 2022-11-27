2845debf694fa9a2e9c574c695598d1c02a2410b
Anastasia <anromanova.design@gmail.com>
2022-11-27 18:12:03 +0100

    test localstorage

--- src/common/visitedPages.test.ts
+++ src/common/visitedPages.test.ts
@@ -5,3 +5,15 @@ test("Test to see visitedPages.addPage() sets localStorage to the param value", 
   visitedPages.addPage(param);
   expect(localStorage.getItem(param)).toBeDefined();
 });
+
+test("Test to see visitedPage.checkPage() returns true if param exists", () => {
+  let param = "Hello";
+  visitedPages.addPage(param);
+  expect(visitedPages.checkPage(param)).toBe(true);
+});
+
+test("Test to see visitedPage.checkPage() returns false if there is no test string", () => {
+  let param = "Hello";
+  visitedPages.addPage(param);
+  expect(visitedPages.checkPage("test")).toBe(false);
+});
