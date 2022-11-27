c479acea8bf2836e424d7ee163b79ea63f95819f
Anastasia <anromanova.design@gmail.com>
2022-11-27 18:57:30 +0100

    bad page test

--- src/components/app/App.test.tsx
+++ src/components/app/App.test.tsx
@@ -1,10 +1,20 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 
 describe("<App />", () => {
   it("Renders <App /> component correctly", () => {
     const { getByText } = render(<App />, { wrapper: BrowserRouter });
     expect(getByText(/Artsysearcher/i)).toBeInTheDocument();
+  });
+
+  test("landing on a bad page", () => {
+    const badRoute = "/bad/route";
+    render(
+      <MemoryRouter initialEntries={[badRoute]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText(/Nothing to see here!/i)).toBeInTheDocument();
   });
 });
